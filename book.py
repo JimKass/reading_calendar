636af66094647720c94147e5180bc14271c07c30
JimKass <james.kastner@my.jcu.edu.au>
2017-10-08 14:34:10 +1000

    Initial commit

---
+++ book.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return "{} by {}; pages: {}".format(self.title, self.author, self.pages)
+
+    
